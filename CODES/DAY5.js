ccf4067ffd1ed6ff0ae8ad0a7ca09e7dd4f7c7cc
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-01-29 21:07:25 +0530

    all code done

---
+++ CODES/DAY5.js
@@ -0,0 +1,35 @@
+let prompt = require("prompt-sync")();
+
+
+// let computer = Math.floor(Math.random() * 100 ) + 1;
+// let user ;  
+// let attempt = 0; 
+// do{
+//   attempt ++;
+//   user = Number(prompt("Enter Your Number to guess the computer Number : -"));
+  
+//   if(user > computer) console.log(" To large:-");
+//   else if(user < computer ) console.log("To Small ");
+//   else if(user === computer) console.log("Congratulation Your Guessed Number are correctly and You can take total",attempt );
+//   else console.log("Invalid Number :-");
+
+// } while(user != computer);
+
+// Automorphic Number :-
+// console.log(Math.floor(Math.random() * 9000) + 1000);
+
+// Q Leep Year
+// let n = Number(prompt("Enter Your Number :-"));
+// if(n % 4 === 0 && n % 100 != 0) console.log("leap year");
+// else if(n % 100 != 0) console.log(" Not a Leap Year");
+// else console.log("Not a Leap Year");
+
+// Q Electricity Bill
+// let unit = Number(prompt("Enter Unit :- "));
+// let amount = 0;
+// if(unit > 0 && unit <= 100) console.log(Math.floor(unit * 4.5));
+// else if(unit > 101 && unit <=200) console.log(Math.floor(unit * 4.5) + (unit * 6));
+// else if(unit > 201 && unit <= 400) console.log(Math.floor(unit * 4.5) + (unit * 6) + (unit + 8));
+// else console.log(Math.floor((unit * 4.5)) + Math.floor((unit * 6)) + Math.floor((unit * 8))+ Math.floor((unit * 13)));
+
+
